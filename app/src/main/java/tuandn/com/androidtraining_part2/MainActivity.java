f28cfc524864a5e3c3dee09cf11a8b1279ed4ac9
TuanDN2 <kingdragon102@gmail.com>
2015-07-06 16:18:40 +0700

    Snackbar

--- app/src/main/java/tuandn/com/androidtraining_part2/MainActivity.java
+++ app/src/main/java/tuandn/com/androidtraining_part2/MainActivity.java
@@ -3,14 +3,16 @@ package tuandn.com.androidtraining_part2;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-
+import android.view.View;
 
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
@@ -52,6 +54,15 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         mDrawerToggle.syncState();
 
         navigate(mNavItemId);
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
     }
 
     private void navigate(final int itemId) {
@@ -64,6 +75,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         menuItem.setChecked(true);
         mNavItemId = menuItem.getItemId();
 
+        View v= new View(MainActivity.this);
+        Snackbar.make(findViewById(R.id.content), menuItem.getTitle() + " clicked", Snackbar.LENGTH_LONG)
+                .show();
         // allow some time after closing the drawer before performing real navigation
         // so the user can see what is happening
         mDrawerLayout.closeDrawer(GravityCompat.START);
@@ -87,6 +101,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         if (item.getItemId() == android.support.v7.appcompat.R.id.home) {
             return mDrawerToggle.onOptionsItemSelected(item);
         }
+        View v= new View(getApplicationContext());
+        Snackbar.make(v, item.getTitle() + " clicked", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
         return super.onOptionsItemSelected(item);
     }
 
