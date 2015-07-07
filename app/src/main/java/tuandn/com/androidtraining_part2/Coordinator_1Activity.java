847d756dc04eb8cdb20e34a4866faf4eae66865b
TuanDN2 <kingdragon102@gmail.com>
2015-07-07 14:50:32 +0700

    Coordinator (Not Finished)

---
+++ app/src/main/java/tuandn/com/androidtraining_part2/Coordinator_1Activity.java
@@ -0,0 +1,15 @@
+package tuandn.com.androidtraining_part2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Anh Trung on 7/7/2015.
+ */
+public class Coordinator_1Activity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.coordinator_1);
+    }
+}
